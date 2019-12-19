b09ed0c265ebd4d95d41c54927c457fb92492610
Katherine Wang <katherine.wang429@gmail.com>
2019-12-19 15:29:47 -0800

    Add morgan middleware

--- server/index.js
+++ server/index.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const parser = require('body-parser');
-const dab = require("../database/index.js");
+const dab = require('../database/index.js');
+const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = 3002;
 
+app.use(morgan('dev'));
 app.use(parser.json());
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
-app.get("/searchbar/:id", (req, res) => {
-  console.log(req.params);
-  dab.getProducts(req.params.id, (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(404).send("error retrieving products");
-    } else {
-      res.status(200).send(data);
-    }
-  });
+app.get('/searchbar/:id', (req, res) => {
+	console.log(req.params);
+	dab.getProducts(req.params.id, (err, data) => {
+		if (err) {
+			console.error(err);
+			res.status(404).send('error retrieving products');
+		} else {
+			res.status(200).send(data);
+		}
+	});
 });
 
 app.listen(PORT, () => {
-  console.log(`server is listening on post apocalyptic ${PORT}`);
+	console.log(`server is listening on post apocalyptic ${PORT}`);
 });
