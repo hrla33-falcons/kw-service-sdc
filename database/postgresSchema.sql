b490165158b352396c6d96c4844ff67610dc62bf
Katherine Wang <katherine.wang429@gmail.com>
2019-12-21 11:06:16 -0800

    10M seed function and postgres schema

---
+++ database/postgresSchema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS ikea;
+
+CREATE DATABASE ikea;
+
+USE ikea;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  name TEXT NOT NULL,
+  type TEXT NOT NULL,
+  dimensions TEXT
+  PRIMARY KEY (id)
+);
